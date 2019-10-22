c8b71c3422a19e7fd6bc86f3119a5c3da1c54c16
Ed Grosvenor <ed.grosvenor@prephero.com>
2019-10-22 14:53:40 -0500

    Add extractor so I can actually do the pull request

---
+++ tests/Unit/IsacWymansideTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use DomDocument;
+use App\Actions\Extractors\IsacWymanside;
+
+/**
+ * Do not edit or remove comment:
+ * Extractor Based On: https://uclabruins.com/staff.aspx
+ */
+class IsacWymansideTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->body = file_get_contents(storage_path('test_pages/IsacWymanside.html'));
+        $this->tidy_config = ['clean' => 'yes', 'output-html' => 'yes'];
+    }
+
+    public function test_contacts_found()
+    {
+        $html = tidy_parse_string($this->body, $this->tidy_config, 'utf8');
+
+        $dom = new DOMDocument;
+
+        libxml_use_internal_errors(true);
+
+        $dom->loadHTML($html);
+
+        $extract = new IsacWymanside($dom, 'https://uclabruins.com/staff.aspx');
+        $extract->execute();
+
+        $this->assertInstanceOf('Illuminate\Support\Collection', $extract->contacts);
+        $this->assertEquals(268, $extract->contacts->where('email', '<>', '')->count());
+        $this->assertEquals(40, count($extract->meta['team_phones']));
+
+
+    }
+}
